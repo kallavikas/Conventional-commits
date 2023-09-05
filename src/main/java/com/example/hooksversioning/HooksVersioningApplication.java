cc6dad28dfc6ad4c99eea22686cd68259db6dac9
vikas-kalla_hmgroup <vikas.kalla+hmgroup@hm.com>
2023-09-05 18:53:37 +0100

    feat: add new user registration feature main

--- src/main/java/com/example/hooksversioning/HooksVersioningApplication.java
+++ src/main/java/com/example/hooksversioning/HooksVersioningApplication.java
@@ -12,5 +12,6 @@ public class HooksVersioningApplication {
         // 3. third commit test1test1
         SpringApplication.run(HooksVersioningApplication.class, args);
         // 4. fourth Line
+        // 5. fifth Line
     }
 }
